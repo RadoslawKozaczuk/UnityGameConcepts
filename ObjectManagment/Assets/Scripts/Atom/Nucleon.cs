7c7a6a70e197d3b222dae16b90930a837592dd21
Radosław Kozaczuk <ko.rad@o2.pl>
2018-09-29 11:46:10 +1000

    ObjectManagment - Neutron scene added, FPS counter prefab added

---
+++ ObjectManagment/Assets/Scripts/Atom/Nucleon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Nucleon : MonoBehaviour
+{
+
+    public float attractionForce;
+
+    Rigidbody _body;
+
+    void Awake()
+    {
+        _body = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        _body.AddForce(transform.localPosition * -attractionForce);
+    }
+}
---
+++ ObjectManagment/Assets/Scripts/Atom/NucleonSpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour
+{
+
+    public float timeBetweenSpawns;
+
+    public float spawnDistance;
+
+    public Nucleon[] nucleonPrefabs;
+
+    float timeSinceLastSpawn;
+
+    // Using FixedUpdate keeps the spawning independent of the frame rate.
+    // If the configured time between spawns is shorter than the frame time, using Update would cause spawn delays.
+    // And as the point of this scene is to tank our frame rate, that will happen.
+   void FixedUpdate()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+        if(timeSinceLastSpawn >= timeBetweenSpawns)
+        {
+            timeSinceLastSpawn -= timeBetweenSpawns;
+            SpawnNucleon();
+        }
+    }
+
+    void SpawnNucleon()
+    {
+        Nucleon prefab = nucleonPrefabs[Random.Range(0, nucleonPrefabs.Length)];
+        Nucleon spawn = Instantiate<Nucleon>(prefab);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
