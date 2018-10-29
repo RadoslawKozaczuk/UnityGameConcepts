93636cd56febd21f4b658f25fdf7adc61b4e7dd8
Radosław Kozaczuk <ko.rad@o2.pl>
2018-10-29 22:52:07 +1100

    Hex - initial commit and first objects

---
+++ Hex Grid Generator/Assets/Scripts/HexGrid.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class HexGrid : MonoBehaviour
+{
+    public HexCell Cell;
+    public int Width = 6;
+    public int Height = 6;
+    
+    HexCell[] _cells;
+
+    void Awake()
+    {
+        _cells = new HexCell[Height * Width];
+
+        for (int z = 0, i = 0; z < Height; z++)
+            for (int x = 0; x < Width; x++)
+                CreateCell(x, z, i++);
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = x * 10f;
+        position.y = 0f;
+        position.z = z * 10f;
+
+        HexCell cell = _cells[i] = Instantiate(Cell);
+        cell.transform.SetParent(transform, false);
+        cell.transform.localPosition = position;
+    }
+}
---
+++ Hex Grid Generator/Assets/Scripts/HexMetrics.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public static class HexMetrics
+{
+    // Hexagon's anatomy
+    // the edge's length (so also the distance from the center to any corner) is equal to 10
+    // the outer radius is equal to 10 as well
+    public const float OuterRadius = 10f;
+    // the inner radius is equal to sqrt(3)/2 times the outer radius
+    public const float InnerRadius = OuterRadius * 0.866025404f;
+
+    public static Vector3[] corners = {
+        new Vector3(0f, 0f, OuterRadius),
+        new Vector3(InnerRadius, 0f, 0.5f * OuterRadius),
+        new Vector3(InnerRadius, 0f, -0.5f * OuterRadius),
+        new Vector3(0f, 0f, -OuterRadius),
+        new Vector3(-InnerRadius, 0f, -0.5f * OuterRadius),
+        new Vector3(-InnerRadius, 0f, 0.5f * OuterRadius)
+    };
+}
