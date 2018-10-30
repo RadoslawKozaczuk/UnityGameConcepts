bd76e8fd66f8b8c3142857ce856e5db108bd5da7
Radosław Kozaczuk <ko.rad@o2.pl>
2018-10-30 18:29:45 +1100

    Hex - cells positioned correctly

--- Hex Grid Generator/Assets/Scripts/HexGrid.cs
+++ Hex Grid Generator/Assets/Scripts/HexGrid.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class HexGrid : MonoBehaviour
 {
     public HexCell Cell;
+    public Text CellLabelPrefab;
     public int Width = 6;
     public int Height = 6;
     
     HexCell[] _cells;
+    Canvas _gridCanvas;
 
     void Awake()
     {
+        _gridCanvas = GetComponentInChildren<Canvas>();
         _cells = new HexCell[Height * Width];
 
         for (int z = 0, i = 0; z < Height; z++)
@@ -20,12 +24,21 @@ public class HexGrid : MonoBehaviour
     void CreateCell(int x, int z, int i)
     {
         Vector3 position;
-        position.x = x * 10f;
+
+        // Each row is offset along the X axis by the inner radius so we have to add half of z to x.
+        // Every second row, all cells should move back one additional step. 
+        // Subtracting the integer division of Z by 2 before multiplying will do the trick.
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.InnerRadius * 2f);
         position.y = 0f;
-        position.z = z * 10f;
+        position.z = z * (HexMetrics.OuterRadius * 1.5f);
 
         HexCell cell = _cells[i] = Instantiate(Cell);
         cell.transform.SetParent(transform, false);
         cell.transform.localPosition = position;
+
+        Text label = Instantiate(CellLabelPrefab);
+        label.rectTransform.SetParent(_gridCanvas.transform, false);
+        label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
+        label.text = x.ToString() + "\n" + z.ToString();
     }
 }
---
+++ Hex Grid Generator/Assets/Scripts/HexMesh.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class HexMesh : MonoBehaviour
+{
+    Mesh _hexMesh;
+    List<Vector3> _vertices;
+    List<int> _triangles;
+
+    void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = _hexMesh = new Mesh();
+        _hexMesh.name = "Hex Mesh";
+        _vertices = new List<Vector3>();
+        _triangles = new List<int>();
+    }
+}
