9dc5e2aaae320e8b3bfb218337e434249f126c70
Radoslaw <ko.rad@o2.pl>
2018-11-14 21:55:39 +1100

    Hex - camera rotation

--- Hex Grid Generator/Assets/Scripts/HexMapCamera.cs
+++ Hex Grid Generator/Assets/Scripts/HexMapCamera.cs
@@ -5,10 +5,12 @@ public class HexMapCamera : MonoBehaviour
     [SerializeField] float _stickMinZoom = -250, _stickMaxZoom = -45;
     [SerializeField] float _swivelMinZoom = 90, _swivelMaxZoom = 45;
     [SerializeField] float _moveSpeedMinZoom = 400, _moveSpeedMaxZoom = 100;
+    [SerializeField] float _rotationSpeed = 180;
     [SerializeField] HexGrid _grid;
 
     Transform _swivel, _stick;
     float _zoom = 1f;
+    float _rotationAngle;
 
     void Awake()
     {
@@ -26,10 +28,26 @@ public class HexMapCamera : MonoBehaviour
         if (zoomDelta != 0f)
             AdjustZoom(zoomDelta);
 
+        float rotationDelta = Input.GetAxis("Rotation");
+        if (rotationDelta != 0f)
+            AdjustRotation(rotationDelta);
+
         float xDelta = Input.GetAxis("Horizontal");
         float zDelta = Input.GetAxis("Vertical");
         if (xDelta != 0f || zDelta != 0f)
             AdjustPosition(xDelta, zDelta);
+    }
+
+    void AdjustRotation(float delta)
+    {
+        _rotationAngle += delta * _rotationSpeed * Time.deltaTime;
+
+        if (_rotationAngle < 0f)
+            _rotationAngle += 360f;
+        else if (_rotationAngle >= 360f)
+            _rotationAngle -= 360f;
+
+        transform.localRotation = Quaternion.Euler(0f, _rotationAngle, 0f);
     }
 
     void AdjustPosition(float xDelta, float zDelta)
