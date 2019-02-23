03b4bda61ffedb65560747103526a2cd569b7731
Radoslaw <ko.rad@o2.pl>
2019-02-23 17:16:37 +0100

    Hex - additional pathfinder improvements
    changed pathfinder methods to recieve cell Ids instead of cell references

--- Hex Grid Generator/Assets/Scripts/Pathfinder.cs
+++ Hex Grid Generator/Assets/Scripts/Pathfinder.cs
@@ -17,13 +17,12 @@ public class Pathfinder
 		}
 	}
 
-	public int Count { get; private set; } = 0;
-
 	readonly List<int> _priorityQueue = new List<int>();
 	readonly InternalData[] _internalData;
 	readonly HexCell[] _cells;
 
 	int _minimum = int.MaxValue;
+	int _count;
 
 	public Pathfinder(HexCell[] cells)
 	{
@@ -43,11 +42,11 @@ public class Pathfinder
 
 		Clear();
 		fromCell.Distance = 0;
-		Enqueue(fromCell, _internalData[fromCell.Id].SearchHeuristic);
+		Enqueue(fromCell.Id, _internalData[fromCell.Id].SearchHeuristic);
 
-		while (Count > 0)
+		while (_count > 0)
 		{
-			HexCell current = Dequeue();
+			HexCell current = _cells[Dequeue()];
 
 			if(current == null)
 				break;
@@ -93,25 +92,26 @@ public class Pathfinder
 					neighbor.Distance = distance;
 					neighbor.PathFrom = current;
 					_internalData[neighbor.Id].SearchHeuristic = searchHeuristic;
-					Enqueue(neighbor, searchHeuristic);
+					Enqueue(neighbor.Id, searchHeuristic);
 				}
 				else if (distance < neighbor.Distance)
 				{
 					float oldPriority = neighbor.Distance + searchHeuristic;
 					neighbor.Distance = distance;
 					neighbor.PathFrom = current;
-					Change(neighbor, searchHeuristic, oldPriority);
+					Change(neighbor.Id, searchHeuristic, oldPriority);
 				}
 			}
 		}
 
+		// no path found
 		return null;
 	}
 
-	void Enqueue(HexCell cell, float searchHeuristic)
+	void Enqueue(int cellId, float searchHeuristic)
 	{
-		Count += 1;
-		int priority = Mathf.RoundToInt(cell.Distance + searchHeuristic);
+		_count += 1;
+		int priority = Mathf.RoundToInt(_cells[cellId].Distance + searchHeuristic);
 		if (priority < _minimum)
 			_minimum = priority;
 
@@ -122,15 +122,15 @@ public class Pathfinder
 			_priorityQueue.Add(int.MinValue);
 
 		// linked list
-		_internalData[cell.Id].NextWithSamePriorityId = _priorityQueue[priority];
+		_internalData[cellId].NextWithSamePriorityId = _priorityQueue[priority];
 
 		// When a cell is added to the queue, let's start by simply using its priority as its index, treating the list as a simple array.
-		_priorityQueue[priority] = cell.Id;
+		_priorityQueue[priority] = cellId;
 	}
 
-	HexCell Dequeue()
+	int Dequeue()
 	{
-		Count -= 1;
+		_count -= 1;
 
 		for (; _minimum < _priorityQueue.Count; _minimum++)
 		{
@@ -139,60 +139,58 @@ public class Pathfinder
 			if (id == int.MinValue)
 				continue;
 
-			HexCell cell = _cells[id];
-			if (cell != null)
-			{
-				_priorityQueue[_minimum] = _internalData[cell.Id].NextWithSamePriorityId;
-				return cell;
-			}
+			_priorityQueue[_minimum] = _internalData[id].NextWithSamePriorityId;
+			return id;
 		}
 
-		return null;
+		// priority queue depleted
+		return int.MinValue;
 	}
 
 	void Clear()
 	{
+		// internal data list is always overwritten before accessed therefore no need for cleanup
 		for (int i = 0; i < _cells.Length; i++)
 			_cells[i].Distance = int.MaxValue;
 
-		Count = 0;
+		_count = 0;
 		_priorityQueue.Clear();
 		_minimum = int.MaxValue;
 	}
 
-	void Change(HexCell cell, float searchHeuristic, float oldPriority)
+	void Change(int cellId, float searchHeuristic, float oldPriority)
 	{
-		var index = Mathf.RoundToInt(oldPriority);
+		int index = Mathf.RoundToInt(oldPriority);
 		// Declaring the head of the old priority list to be the current cell, and also keep track of the next cell.
 		// We can directly grab the next cell, because we know that there is at least one cell at this index.
-		HexCell current = _cells[_priorityQueue[index]];
-		HexCell next = _cells[_internalData[current.Id].NextWithSamePriorityId];
+		int currentCellId = _priorityQueue[index];
+		int nextCellId = _internalData[currentCellId].NextWithSamePriorityId;
 
 		// If the current cell is the changed cell, then it is the head cell and we can cut it away, as if we dequeued it.
-		if (current == cell)
+		if (currentCellId == cellId)
 		{
-			_priorityQueue[index] = next.Id;
+			_priorityQueue[index] = nextCellId;
 		}
 		// If not, we have to follow the chain until we end up at the cell in front of the changed cell.
 		// That one holds the reference to the cell that has been changed.
 		else
 		{
-			while (next != cell)
+			while (nextCellId != cellId)
 			{
-				current = next;
-				var nextId = _internalData[current.Id].NextWithSamePriorityId;
-				next = _cells[nextId];
+				currentCellId = nextCellId;
+				var nextId = _internalData[currentCellId].NextWithSamePriorityId;
+				nextCellId = nextId;
 			}
 
 			// At this point, we can remove the changed cell from the linked list, by skipping it.
-			_internalData[current.Id].NextWithSamePriorityId = _internalData[cell.Id].NextWithSamePriorityId;
+			_internalData[currentCellId].NextWithSamePriorityId = _internalData[cellId].NextWithSamePriorityId;
 		}
 
 		// After the cell has been removed, it has to be added again so it ends up in the list for its new priority.
-		Enqueue(cell, searchHeuristic);
+		Enqueue(cellId, searchHeuristic);
 
 		// The Enqueue method increments the count, but we're not actually adding a new cell.
 		// So we have to decrement the count to compensate for that.
-		Count -= 1;
+		_count -= 1;
 	}
 }
